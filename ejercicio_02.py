561db91fa5562690867a132b13e9b7c53c5f7cd1
bhm1998 <98926989+bhm1998@users.noreply.github.com>
2022-02-19 12:39:48 -0600

    Update ejercicio_02.py

---
+++ ejercicio_02.py
@@ -0,0 +1,7 @@
+def calcular_litros_por_hora(tiempo_de_ciclismo):
+  resultado=(0.5*tiempo_de_ciclismo)
+  print(resultado)
+    
+calcular_litros_por_hora(3)
+calcular_litros_por_hora(6.7)
+calcular_litros_por_hora(11.8)
