4d15f79e630a97912583269bea2935aa5f36148a
bhm1998 <98926989+bhm1998@users.noreply.github.com>
2022-02-20 22:06:35 -0600

    Update ejercicio_04.py

--- ejercicio_04.py
+++ ejercicio_04.py
@@ -1,8 +1,7 @@
+
 def suma_del_saltamontes(n):
-    suma=0
-    for i in range(1,n+1):
-        suma+=i
-        print(suma)
+    suma=sum(range(1,n+1))
+    print(suma)
         
 suma_del_saltamontes(2)
 suma_del_saltamontes(8)
